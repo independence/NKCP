2aa4a1c211e9f4521d96ebc26a56b57367e94db4
Hiennv2 <hiennk90@gmail.com>
2014-11-25 09:42:08 +0700

    Thêm mới form checkIn

--- DataAccess/DatabaseDA.Designer.cs
+++ DataAccess/DatabaseDA.Designer.cs
@@ -1,4 +1,4 @@
-﻿// Default code generation is disabled for model 'D:\22-11-2014-ProjectGoverment\DataAccess\DatabaseDA.edmx'. 
+﻿// Default code generation is disabled for model 'D:\ProjectGoverment-25-11-2014\DataAccess\DatabaseDA.edmx'. 
 // To enable default code generation, change the value of the 'Code Generation Strategy' designer
 // property to an alternate value. This property is available in the Properties Window when the model is
 // open in the designer.
